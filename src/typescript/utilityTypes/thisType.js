be9d74e5d7ccfd8a76d1a87c92fce3b7217ac960
yuzhanglong <yuzl1123@163.com>
2020-12-19 00:59:54 +0800

    refactor(typescript): Utility Types

---
+++ src/typescript/utilityTypes/thisType.js
@@ -0,0 +1,9 @@
+// ThisType<Type> 通过 ThisType
+// 可以方便地控制 this 类型。
+// ps.它只有在 --noImplicitThis 的选项下才有效。
+var helperFunctions = {
+    hello: function () {
+        this.logError("Error: Something went wrong!");
+        this.update(); // 编译时会发现错误：Property 'update' does not exist on type 'HelperThisValue'.
+    }
+};
