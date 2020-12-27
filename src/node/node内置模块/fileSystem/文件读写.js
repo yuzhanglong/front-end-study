971239345b2638629dcb21896e83f2078d555c25
yuzhanglong <yuzl1123@163.com>
2020-12-28 01:25:44 +0800

    feat(node): node 内置模块

---
+++ src/node/node内置模块/fileSystem/文件读写.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.resolve("../test/yzl.txt");
+
+fs.writeFile(filePath, "yzl", {
+  flag: "a"
+}, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
+
+fs.readFile(filePath, {
+  encoding: 'utf8'
+}, (err, data) => {
+  console.log(data);
+});
