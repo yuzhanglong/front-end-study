3597fb5f91dfa44e38d3c12bf94b77306ffa3cd6
yuzhanglong <yuzl1123@163.com>
2020-12-29 22:02:28 +0800

    feat(express): express 中间件

---
+++ src/node/express/入门中间件.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+// 注册中间件
+
+app.use((req, res) => {
+  console.log("注册成功~");
+});
+
+app.listen(8000, () => {
+  console.log("success!");
+})
---
+++ src/node/express/路径方法中间件.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+// 路径 + 方法中间件
+
+app.use((req, res, next) => {
+  console.log("home middleware 01");
+  next();
+});
+
+app.get('/home', (req, res, next) => {
+  console.log("home and method middleware 01");
+});
+
+
+app.listen(8000, () => {
+  console.log("success!");
+})
