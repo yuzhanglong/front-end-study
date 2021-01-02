50ed0ccf95c18d11d6100f389a42a7810a6bced3
yuzhanglong <yuzl1123@163.com>
2021-01-02 15:39:03 +0800

    feat(node): express & koa 源码解读 - (express 源码解读完成)

--- src/node/express/连续注册中间件.js
+++ src/node/express/连续注册中间件.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(/* 中间件 M1 */ (req, res, next) => {
   console.log("middleware 01 in app.use");
   next();
-}, (req, res, next) => {
+}, /* 中间件 M2 */ (req, res, next) => {
   console.log("middleware 02 in app.use");
   next();
 });
 
-app.get('/home', (req, res, next) => {
+app.get('/home', /* 中间件 M3 */ (req, res, next) => {
   console.log("home and get method middleware 01");
   next();
-}, (req, res, next) => {
+}, /* 中间件 M4 */ (req, res, next) => {
   console.log("home and get method middleware 02");
   next();
-}, (req, res, next) => {
+}, /* 中间件 M5 */ (req, res, next) => {
   console.log("home and get method middleware 03");
   res.end("hello world");
 });
