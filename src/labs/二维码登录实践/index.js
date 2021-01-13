e6bb63b8db4450d1f24efa04ef6fbbe94ed18ade
yuzhanglong <yuzl1123@163.com>
2021-01-13 18:44:25 +0800

    feat(js): 扫码登录实践 -- 完成登录逻辑

--- src/labs/二维码登录实践/index.js
+++ src/labs/二维码登录实践/index.js
@@ -2,8 +2,9 @@ const Koa = require('koa');
 const WebSocket = require('ws');
 const Router = require("koa-router");
 const qr = require('qr-image')
+const bodyParser = require('koa-bodyparser');
 const {initWebsocket} = require("./ws/ws");
-
+const cors = require('koa2-cors')
 const myRouter = new Router();
 
 let wss;
@@ -34,37 +35,65 @@ myRouter.get("/login_qr_code/:uid", (ctx) => {
 });
 
 // 执行登录
-myRouter.post("/login_by_code", (ctx) => {
-  const token = "231";
-  const codeId = "21332";
+myRouter.post("/login_by_code", (ctx, next) => {
+  // 用户的 token 来自手机端
+  const token = ctx.request.body["token"];
 
-  // 过期验证逻辑，略去
-  const isExpired = false;
-
-  // token 有效性验证，略去
-  const isTokenSuccess = true;
+  // codeId
+  const codeId = ctx.request.body["codeId"];
 
   if (wss) {
     // 可以在 wss.clients 中找到相应的结果
     const clients = wss.clients;
     // 找到对应的 ws 客户端
-    let targetClient = [...clients].find((client) => client.uid === codeId);
+    let targetClient = [...clients].find((client) => client.qrCodeCondition.uid === codeId);
     if (targetClient) {
-      // 发送新的 token 给客户端，客户端利用它来执行登录
-      targetClient.send("new token");
-      ctx.body = {
-        message: "登录成功"
+      if (targetClient.qrCodeCondition.status === 0) {
+        targetClient.send(JSON.stringify({
+          status: "success",
+          data: {
+            uid: codeId
+          },
+          type: "SCANNED"
+        }));
+        targetClient.qrCodeCondition.status++;
+        ctx.body = {
+          message: "用户已经扫描了二维码，请点击确认按钮以确认登录"
+        }
+      } else if (targetClient.qrCodeCondition.status === 1) {
+        // 派发新的token
+        targetClient.send(JSON.stringify({
+          status: "success",
+          data: {
+            uid: codeId
+          },
+          type: "SUCCESS"
+        }));
+
+        ctx.body = {
+          message: "登录成功~",
+          data: {
+            token: "NEW TOKEN"
+          }
+        }
+        targetClient.qrCodeCondition.status++;
+      } else {
+        ctx.body = {
+          message: "二维码已经失效！"
+        }
       }
+      return;
     }
-  } else {
-    ctx.body = {
-      message: "登录失败"
-    }
+  }
+  ctx.body = {
+    message: "登录失败"
   }
 });
 
 const app = new Koa();
 
+app.use(cors());
+app.use(bodyParser());
 app.use(myRouter.routes());
 
 const server = app.listen(8000, () => {
