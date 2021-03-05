5f0e8b76b18eac0e64540b05e8b5d554b94932a5
coderyzl <yuzl1123@163.com>
2021-03-06 01:14:08 +0800

    :sparkles: feat(labs): 完成组件国际化 lab DEMO

--- src/labs/react-local/src/App.tsx
+++ src/labs/react-local/src/App.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
+/*
+ * File: App.tsx
+ * Description: App 组件
+ * Created: 2021-3-6 00:57:33
+ * Author: yuzhanglong
+ * Email: yuzl1123@163.com
+ */
+
+import React from 'react'
+import LocalProvider from './local/LocalProvider'
+import Button from './components/Button'
+import Chinese from './languages/chinese'
 
 interface AppProps {
 
@@ -6,9 +17,13 @@ interface AppProps {
 
 const App: React.FunctionComponent<AppProps> = () => {
   return (
-    <div>
-    </div>
+    <LocalProvider local={Chinese}>
+      <div>
+        <Button />
+      </div>
+    </LocalProvider>
+
   )
 }
 
-export default App;
+export default App
