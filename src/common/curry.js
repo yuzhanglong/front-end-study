aff476cfa43a9ea3f76e6f7dc76c16242cd7888b
coderyzl <yuzl1123@163.com>
2021-03-06 19:07:45 +0800

    :sparkles: refactor: 优化 使用 postMessage 跨域的 DEMO

--- src/common/curry.js
+++ src/common/curry.js
@@ -1,33 +1,33 @@
-// function f(num) {
-//   if (!this.number) {
-//     this.number = 0;
-//   }
-//   if (num) {
-//     this.number += num;
-//     return f.bind(this);
-//   } else {
-//     console.log(this.number);
-//     this.number = undefined;
-//   }
-// }
-//
-// f(1)() // 1
-//
-// f(1)(2)(3)() // 6
-//
-// f(1)(2)(3)(4)() // 10
+function f(num) {
+  if (!this.number) {
+    this.number = 0
+  }
+  if (num) {
+    this.number += num
+    return f.bind(this)
+  } else {
+    console.log(this.number)
+    this.number = undefined
+  }
+}
+
+f(1)() // 1
+
+f(1)(2)(3)() // 6
+
+f(1)(2)(3)(4)() // 10
 
 
-// const f2 = (num, sum = 0) => {
-//   if (!num) {
-//     console.log(sum);
-//   } else {
-//     return (arg) => {
-//       return f2(arg, sum + num);
-//     }
-//   }
-// }
-//
-// f2(1)() // 1
-// f2(1)(2)(3)() // 6
-// f2(1)(2)(3)(4)() // 10
+const f2 = (num, sum = 0) => {
+  if (!num) {
+    console.log(sum)
+  } else {
+    return (arg) => {
+      return f2(arg, sum + num)
+    }
+  }
+}
+
+f2(1)() // 1
+f2(1)(2)(3)() // 6
+f2(1)(2)(3)(4)() // 10
