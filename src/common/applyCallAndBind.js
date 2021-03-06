8a6235fb5181828d312d5f4cc9c3c1b24d3cad52
coderyzl <yuzl1123@163.com>
2021-03-06 19:07:45 +0800

    :sparkles: refactor: 优化 使用 postMessage 跨域的 DEMO

--- src/common/applyCallAndBind.js
+++ src/common/applyCallAndBind.js
@@ -1,40 +1,44 @@
-Function.prototype.myCall = function (thisArg, ...args) {
-  const fn = this;
+Function.prototype.myCall = function(thisArg, ...args) {
+  const fn = this
 
   if (thisArg === null || thisArg === undefined) {
-    return fn(...args);
+    return fn(...args)
   }
 
-  const fnKey = Symbol("function key");
-  thisArg[fnKey] = fn;
-  let res = thisArg[fnKey](...args);
-  Reflect.deleteProperty(thisArg, fnKey);
-  return res;
+  const fnKey = Symbol('function key')
+  thisArg[fnKey] = fn
+  let res = thisArg[fnKey](...args)
+  Reflect.deleteProperty(thisArg, fnKey)
+  return res
 }
 
-Function.prototype.myApply = function (thisArg, args) {
-  const fn = this;
+Function.prototype.myApply = function(thisArg, args) {
+  const fn = this
 
   if (thisArg === null || thisArg === undefined) {
-    return fn(args);
+    return fn(args)
   }
 
-  const fnKey = Symbol("function key");
-  thisArg[fnKey] = fn;
-  let res = thisArg[fnKey](args);
-  Reflect.deleteProperty(thisArg, fnKey);
-  return res;
+  // 防止成员变量冲突
+  const fnKey = Symbol('function key')
+  thisArg[fnKey] = fn
+
+  let res = thisArg[fnKey](args)
+  Reflect.deleteProperty(thisArg, fnKey)
+  return res
 }
 
-Function.prototype.myBind = function (thisArg, ...args) {
-  const fn = this;
-  const boundFn = function (...otherArgs) {
-    return fn.call(new.target ? this : thisArg, ...args, ...otherArgs);
+Function.prototype.myBind = function(thisArg, ...args) {
+  const fn = this
+  const boundFn = function(...otherArgs) {
+    // 请参阅：https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new.target
+    // new.target属性允许你检测函数或构造方法是否是通过 new 运算符被调用的
+    return fn.call(new.target ? this : thisArg, ...args, ...otherArgs)
   }
   // 无论何时，只要创建一个函数，就会按照特定的规则为这个函数创建一个 prototype 属性（指向原型对象）
-  boundFn.prototype = Object.create(fn.prototype);
+  boundFn.prototype = Object.create(fn.prototype)
   // 默认情况下，所有原型对象自动获得一个名为 constructor 的属性，指回与之关联的构造函数
-  boundFn.prototype.constructor = boundFn;
+  boundFn.prototype.constructor = boundFn
 
   Object.defineProperties(boundFn, {
     name: {
@@ -44,5 +48,5 @@ Function.prototype.myBind = function (thisArg, ...args) {
       value: fn.length
     }
   })
-  return boundFn;
+  return boundFn
 }
---
+++ src/labs/postMessage/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(fs.readFileSync(path.resolve(process.cwd(), 'remote.html')).toString())
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
