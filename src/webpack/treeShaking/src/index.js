3bccc88707e27e0739ed5941ef815c9e27ba07e1
coderyzl <yuzl1123@163.com>
2021-03-07 15:06:46 +0800

    :sparkles: feat(webpack): 添加 treeShaking demo

---
+++ src/webpack/treeShaking/src/index.js
@@ -0,0 +1,17 @@
+import { cube } from './math.js'
+import './myModule'
+
+const component = () => {
+  const element = document.createElement('pre')
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n')
+
+  return element
+}
+
+const pure = /*#__PURE__*/ hello();
+
+document.body = component()
