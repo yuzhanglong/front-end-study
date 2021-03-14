e956f08ee2ae02a7b649b5670077205609209f46
yuzhanglong <yuzl1123@163.com>
2021-03-15 01:39:39 +0800

    :sparkles: feat: update eventEmitter demo

--- src/node/node内置模块/events/basic.js
+++ src/node/node内置模块/events/basic.js
@@ -1,24 +1,44 @@
-const EventEmitter = require("events");
+const EventEmitter = require('events')
 
-const emitter = new EventEmitter();
+const emitter = new EventEmitter()
 
-emitter.on("click", (args) => {
-  console.log(args);
-  console.log("click!");
-});
+emitter.on('click', (args) => {
+  console.log(args)
+  console.log('click!')
+})
 
-emitter.on("click", (args) => {
-  console.log(args);
-  console.log("click2!");
-});
+emitter.on('click', (args) => {
+  console.log(args)
+  console.log('click2!')
+})
 
-emitter.on("hello", (args) => {
-  console.log(args);
-  console.log("hello");
-});
+emitter.on('hello', (args) => {
+  console.log(args)
+  console.log('hello')
+})
 
 
-emitter.emit("click", "yzl");
+emitter.emit('click', 'yzl')
 
-console.log(emitter.eventNames());
-console.log(emitter.listenerCount("click"));
+console.log(emitter.eventNames())
+console.log(emitter.listenerCount('click'))
+
+
+// 继承方式使用 EventEmitter 很多的 js 库偏爱这种方式
+class MyEvent extends EventEmitter {
+  constructor() {
+    super()
+  }
+
+  sayHello() {
+    this.emit('hello')
+  }
+}
+
+const e = new MyEvent()
+
+e.on('hello', () => {
+  console.log('event hello!')
+})
+
+e.sayHello()
