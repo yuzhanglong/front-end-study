a46215e198103d5e3a4be56bc7656ff7c958a66d
yuzhanglong <yuzl1123@163.com>
2021-03-21 00:00:47 +0800

    :art: feat: add Object useful methods demos

---
+++ src/common/objectMethods/is.spec.js
@@ -0,0 +1,16 @@
+describe('Object.is', function() {
+  test('Object.is() 方法判断两个值是否为同一个值', () => {
+    expect(Object.is(undefined, undefined)).toBeTruthy()
+    expect(Object.is(null, undefined)).toBeFalsy()
+    expect(Object.is(null, null)).toBeTruthy()
+    expect(Object.is(true, true)).toBeTruthy()
+    expect(Object.is(false, false)).toBeTruthy()
+    expect(Object.is(+0, -0)).toBeFalsy()
+    expect(Object.is(0, 0)).toBeTruthy()
+    expect(Object.is(+0, +0)).toBeTruthy()
+    expect(Object.is('hello', 'hello')).toBeTruthy()
+    expect(Object.is({}, {})).toBeFalsy()
+    expect(Object.is(NaN, NaN)).toBeTruthy()
+    expect(Object.is(Number.NaN, NaN)).toBeTruthy()
+  })
+})
