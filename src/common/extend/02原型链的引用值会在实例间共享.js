ea04c3d7db487a572b481ddf1b528c12b2dfb449
yuzhanglong <yuzl1123@163.com>
2021-04-05 19:41:45 +0800

    refactor: extend

---
+++ src/common/extend/02原型链的引用值会在实例间共享.js
@@ -0,0 +1,18 @@
+function SuperType() {
+  this.colors = ['red', 'blue', 'green']
+}
+
+function SubType() {
+
+}
+
+SubType.prototype = new SuperType()
+
+let instance1 = new SubType()
+instance1.colors.push('black')
+
+let instance2 = new SubType()
+console.log(instance2.colors)
+
+// [ 'red', 'blue', 'green', 'black' ]
+// [ 'red', 'blue', 'green', 'black' ]
