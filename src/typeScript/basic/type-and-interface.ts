773129cd26a496f9a2d38eabc7a6302cc2047086
yuzhanglong <yuzl1123@163.com>
2021-04-24 11:17:02 +0800

    fix: fix wrong description about E-tag

---
+++ src/typeScript/basic/type-and-interface.ts
@@ -0,0 +1,26 @@
+interface User {
+  name: string
+  age: number
+}
+
+interface SetUser {
+  (name: string, age: number): void;
+}
+
+type User2 = {
+  name: string
+  age: number
+}
+
+type SetUser2 = (name: string, age: number) => void;
+
+// 当你想获取一个变量的类型时，使用 typeof
+let div = document.createElement('div')
+type B = typeof div
+
+
+// never 类型收窄
+let a: never;
+
+// a = '1213'
+
