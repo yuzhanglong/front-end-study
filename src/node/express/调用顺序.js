1f4aa351bdbdcb844e3cb88676e4061250047291
yuzhanglong <yuzl1123@163.com>
2021-04-28 15:16:03 +0800

    chore: express core demo

--- src/node/express/调用顺序.js
+++ src/node/express/调用顺序.js
@@ -26,6 +26,48 @@ app.get('/home', (req, res, next) => {
 });
 
 
+app.post('/home', (req, res, next) => {
+  console.log("home and get method middleware 01");
+  next();
+  console.log("after m3 next");
+}, (req, res, next) => {
+  console.log("home and get method middleware 02");
+  next();
+  console.log("after m4 next");
+}, (req, res, next) => {
+  console.log("home and get method middleware 03");
+  res.end("hello world");
+});
+
+app.post('/family', (req, res, next) => {
+  console.log("family and get method middleware 01");
+  next();
+  console.log("after family m3 next");
+}, (req, res, next) => {
+  console.log("family and get method middleware 02");
+  next();
+  console.log("after family m4 next");
+}, (req, res, next) => {
+  console.log("family and get method middleware 03");
+  res.end("hello family");
+});
+app.get('/home/family', (req, res, next) => {
+  console.log("home and get method middleware 01");
+  next();
+  console.log("after m3 next");
+}, (req, res, next) => {
+  console.log("home and get method middleware 02");
+  next();
+  console.log("after m4 next");
+}, (req, res, next) => {
+  console.log("home and get method middleware 03");
+  res.end("hello world");
+});
+
+
+
+
+
 app.listen(8000, () => {
   console.log("your project is running successfully!");
 });
