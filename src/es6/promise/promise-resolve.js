6670dde73165be90a115c243da297b90e739381b
yuzhanglong <yuzl1123@163.com>
2021-04-28 16:19:55 +0800

    chore: promise resolve demo

---
+++ src/es6/promise/promise-resolve.js
@@ -0,0 +1,12 @@
+const p = new Promise((res) => {
+  setTimeout(() => {
+    console.log('hello world')
+    res('hello~')
+  }, 1000)
+})
+
+const t = Promise.resolve(p)
+console.log(t === p)
+t.then(r => {
+  console.log(r)
+})
