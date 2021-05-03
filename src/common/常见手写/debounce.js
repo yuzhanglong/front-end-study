2fd01ea13d784e29d937ab3b49dac684ff695974
yuzhanglong <yuzl1123@163.com>
2021-05-03 14:10:21 +0800

    chore: update

---
+++ src/common/常见手写/debounce.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+function debounce(fn, time) {
+  let timeout
+  return function() {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      fn.apply(this, arguments)
+    }, time)
+  }
+}
