1ba6e68ce66f95d695e8f11040d6f00b8f95cbca
yuzhanglong <yuzl1123@163.com>
2021-05-05 11:16:41 +0800

    feat: update es6 examples

---
+++ src/common/array/flat-map.spec.js
@@ -0,0 +1,13 @@
+describe('flat map', function() {
+  test('flat map 用法', () => {
+    // flatMap 的一个用法是在 map 的过程中删除某一项
+    let arr = [1, -3, 3, 4, -2]
+    let res = arr.flatMap((item) => {
+      if (item < 0) {
+        return []
+      }
+      return item
+    })
+    expect(res).toStrictEqual([1, 3, 4])
+  })
+})
