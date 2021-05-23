2cc183590e78adbb77fd7b71cb064275e31bcc64
yuzhanglong <yuzl1123@163.com>
2021-05-24 01:13:02 +0800

    feat: add abstract Factory Pattern

--- src/design-patterns/工厂模式.spec.ts
+++ src/design-patterns/工厂模式.spec.ts
@@ -1,28 +1,27 @@
 describe('工厂模式', () => {
+  interface VehicleOptions {
+    doors: number
+    state: string
+    color: string
+  }
+
+  // A constructor for defining new cars
+  function Car(options: VehicleOptions) {
+    // some defaults
+    this.doors = options.doors || 4
+    this.state = options.state || 'brand new'
+    this.color = options.color || 'silver'
+
+  }
+
+  // A constructor for defining new trucks
+  function Truck(options: VehicleOptions) {
+    this.doors = options.state || 'used'
+    this.state = options.state || 'brand old'
+    this.color = options.color || 'blue'
+  }
+
   test('工厂模式', () => {
-    interface VehicleOptions {
-      doors: number
-      state: string
-      color: string
-    }
-
-    // A constructor for defining new cars
-    function Car(options: VehicleOptions) {
-      // some defaults
-      this.doors = options.doors || 4
-      this.state = options.state || 'brand new'
-      this.color = options.color || 'silver'
-
-    }
-
-    // A constructor for defining new trucks
-    function Truck(options: VehicleOptions) {
-      this.doors = options.state || 'used'
-      this.state = options.state || 'brand old'
-      this.color = options.color || 'blue'
-    }
-
-
     class Vehicle {
       private static vehicleClass = Car
 
@@ -52,5 +51,39 @@ describe('工厂模式', () => {
 
     expect(res).toBeInstanceOf(Car)
     expect(res2).toBeInstanceOf(Truck)
+  })
+
+  test('抽象工厂模式', () => {
+    class VehicleFactory {
+      private types = {}
+
+      getVehicle(type, customizations) {
+        const Vehicle = this.types[type]
+        return (Vehicle ? new Vehicle(customizations) : null)
+      }
+
+      registerVehicle(type, Vehicle) {
+        (this.types)[type] = Vehicle
+      }
+    }
+
+    const abstractVehicleFactory = new VehicleFactory()
+
+    abstractVehicleFactory.registerVehicle('car', Car)
+    abstractVehicleFactory.registerVehicle('truck', Truck)
+
+    // Instantiate a new car based on the abstract vehicle type
+    const car = abstractVehicleFactory.getVehicle('car', {
+      color: 'lime green',
+      state: 'like new'
+    })
+
+    // Instantiate a new truck in a similar manner
+    const truck = abstractVehicleFactory.getVehicle('truck', {
+      wheelSize: 'medium',
+      color: 'neon yellow'
+    })
+    expect(car).toBeInstanceOf(Car)
+    expect(truck).toBeInstanceOf(Truck)
   })
 })
