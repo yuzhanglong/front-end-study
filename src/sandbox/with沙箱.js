c43c8f1612c5a02d13e563a275db07db5039702e
yuzhanglong <loveyzl1123@gmail.com>
2021-11-18 21:01:49 +0800

    feat: sandbox

---
+++ src/sandbox/with沙箱.js
@@ -0,0 +1,23 @@
+// 执行上下文对象
+const ctx = {
+  func: variable => {
+    console.log(variable)
+  },
+  foo: 'foo'
+}
+
+// 非常简陋的沙箱
+function veryPoorSandbox(code, ctx) {
+  with (ctx) { // Add with
+    eval(code)
+  }
+}
+
+// 待执行程序
+const code = `
+    foo = 'bar'
+    func(foo)
+`
+
+// bar
+veryPoorSandbox(code, ctx)
