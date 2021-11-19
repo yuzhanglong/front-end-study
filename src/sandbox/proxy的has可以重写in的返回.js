44e17c4f4c9338a495afc048c74dff5d5ac691be
yuzhanglong <loveyzl1123@gmail.com>
2021-11-19 23:45:39 +0800

    feat: sandbox2

---
+++ src/sandbox/proxy的has可以重写in的返回.js
@@ -0,0 +1,16 @@
+const a = {
+  name: 'yzl',
+  age: 20
+}
+
+console.log(a)
+
+console.log('name' in a)
+
+const aa = new Proxy(a, {
+  has() {
+    return false
+  }
+})
+
+console.log('name' in aa)
